8baca74f36b9961d80163ddfd91b3a329ef91955
Andrey_Kiselev <fant0m2311@gmail.com>
2023-03-12 17:35:33 +0300

    Ex56 decided

--- Example56/Program.cs
+++ Example56/Program.cs
@@ -9,12 +9,66 @@
 // суммой элементов: 0 строка
 
 
+int GetByteValueFromUser(string message)
+{
+   Console.Write(message);
+   string value = Console.ReadLine()!;
+   bool valid = byte.TryParse(value, out byte f);
+   if (valid == true) return Convert.ToByte(value);
+   else return GetByteValueFromUser(message);
+}
+int[,] CreateMatrix(int size0, int size1, int from = 1, int to = 10)
+{
+   int[,] newArray = new int[size0, size1];
+   for (int i = 0; i < size0; i++)
+   {
+      for (int j = 0; j < size1; j++)
+      {
+         newArray[i, j] = new Random().Next(from, to);
+      }
+   }
+   return newArray;
+}
+string PrintMatrix(int[,] matrix)
+{
+   int rows = matrix.GetLength(0);
+   int columns = matrix.GetLength(1);
+   string result = String.Empty;
+   for (int i = 0; i < rows; i++)
+   {
+      for (int j = 0; j < columns; j++)
+      {
+         result += $"{matrix[i, j]}  ";
+      }
+      result += "\n";
+   }
+   return result;
+}
+int LineMinSumm(int[,] matrix)
+{
+   int rows = matrix.GetLength(0);
+   int columns = matrix.GetLength(1);
+   int currentMin = columns * 100;
+   int result = 0;
+   for (int i = 0; i < rows; i++)
+   {
+      int summa = 0;
+      for (int j = 0; j < columns; j++)
+      {
+         summa += matrix[i, j];
+      }
+      if (currentMin > summa)
+      {
+         result = i;
+         currentMin = summa;
+      }
+   }
+   return result;
+}
 
+int lines = GetByteValueFromUser("Введите количество строк двумерного массива: ");
+int columns = GetByteValueFromUser("Введите количество столбцов двумерного массива: ");
+int[,] mass = CreateMatrix(lines, columns);
 
-
-
-
-
-
-
-Console.WriteLine("Hello, World!");
+Console.WriteLine(PrintMatrix(mass));
+Console.WriteLine($"{LineMinSumm(mass)} строка");
